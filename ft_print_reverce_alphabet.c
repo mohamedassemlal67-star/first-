8bb92b05c4fb868697d764a47e54e3df673c70b6
mohamedassemlal <mohamedassemlal67@gmail.com>
2026-01-16 18:17:39 +0100

    ok

---
+++ ft_print_reverce_alphabet.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void ft_print_reverce_alphbet(void);
+
+int main(void)
+{
+    ft_print_reverce_alphbet();
+}
+
+void ft_print_reverce_alphabet(void)
+{
+    char letter = 'z';
+    while (letter <= 'a')
+    {
+        write(1, &letter, 1);
+        letter--;
+    }
+    
+}
