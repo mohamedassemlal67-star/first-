99285e9484b952c5b80a3c9437ef710cbdd1e72b
mohamedassemlal <mohamedassemlal67@gmail.com>
2026-01-17 22:14:46 +0100

    new function added

---
+++ ft_print_comb.c
@@ -0,0 +1,41 @@
+#include <unistd.h>
+
+void ft_print_comb (void);
+
+void ft_putchar(char c)
+{
+    write(1 ,&c ,1);
+}
+
+
+
+int main (void)
+{
+
+}
+
+void ft_print_comb (void)
+{
+    char a,b,c;
+    for(a = '0'; a <= '7'; a++)
+    {
+        for ( b = 0; b <= '8'; b++)
+        {
+            for ( c = 0; c < '9'; c++)
+            {
+                ft_putchar('a');
+                ft_putchar('b');
+                ft_putchar('c');
+                if (!(a == '7' && b == '8' && c == '9'))
+                {
+                    ft_putchar(',');
+                    ft_putchar(' ');
+                }
+                
+            }
+            
+        }
+        
+    }
+
+}
