41164533e0453bac72a21cf6480fc59ad62605a8
mohamedassemlal <mohamedassemlal67@gmail.com>
2026-01-18 18:33:06 +0100

    initial commit

--- ft_print_comb2.c
+++ ft_print_comb2.c
@@ -2,6 +2,10 @@
 
 void ft_print_comb2 (void);
 
+void ft_putchar (char c);
+
+void ft_put2digits (int n);
+
 int main (void)
 {
     ft_print_comb2();
@@ -12,35 +16,33 @@ void ft_putchar (char c)
     write (1, &c ,1);
 }
 
+void ft_put2digits (int n)
+{
+    ft_putchar((n / 10) + '0');
+	ft_putchar((n % 10) + '0');
+}
+
 void ft_print_comb2 (void)
 {
-    char a,b,c,d;
-    for ( a = '0'; a <= '0'; a++)
+    int a,b;
+    for ( a = 0; a <= 98; a++)
     {
-        for ( b = a + 1; b <= '8'; b++)
+        for ( b = a + 1; b <= 99; b++)
         {
-            for ( c = b + 1; c <= '9'; c++)
+            ft_put2digits(a);
+            ft_putchar(' ');
+            ft_put2digits(b);
+            if (!(a == 98 && b == 99))
             {
-                for ( d = c + 1; d <= '9'; d++)
-                {
-                    ft_putchar(a);
-                    ft_putchar(b);
-                    ft_putchar(' ');
-                    ft_putchar(c);
-                    ft_putchar(d);
-                    if (!(a == '0' && b == '8' && c == '9' && d == '9'))
-                    {
-                        ft_putchar(',');
-                        ft_putchar(' ');
-                        ft_putchar(' ');
-                    }
-                    
-                }
-                
+                ft_putchar(',');
+                ft_putchar(' ');
+                ft_putchar(' ');
             }
-            
+                    
         }
-        
+                
     }
-    
+            
 }
+        
+    
